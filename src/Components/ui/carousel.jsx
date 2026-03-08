2fecbe05802220ddd8839b663b207ac1e5c429c1
MEZBAH-BIN-ANAM <157868848+MEZBAH-BIN-ANAM@users.noreply.github.com>
2026-03-08 19:36:43 +0600

    Update carousel.jsx

--- src/Components/ui/carousel.jsx
+++ src/Components/ui/carousel.jsx
@@ -3,7 +3,7 @@ import useEmblaCarousel from "embla-carousel-react";
 import { ArrowLeft, ArrowRight } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Button } from "@/Components/ui/button"
 
 const CarouselContext = React.createContext(null)
 
@@ -192,3 +192,4 @@ function CarouselNext({
 }
 
 export { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext };
+
