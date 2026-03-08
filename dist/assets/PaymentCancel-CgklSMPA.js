c1db63ac11c95ba7ab1c3a0891b32911d3a04059
MEZBAH-BIN-ANAM <157868848+MEZBAH-BIN-ANAM@users.noreply.github.com>
2026-03-08 15:53:46 +0600

    Add files via upload

---
+++ dist/assets/PaymentCancel-CgklSMPA.js
@@ -0,0 +1,9 @@
+import{j as e,L as t}from"./index-Dj_ChS2e.js";const x=()=>e.jsx("div",{className:"h-full pt-12 pb-10  max-[325px]:px-1 px-4 flex items-center justify-center bg-linear-to-br from-gray-100 via-white to-gray-200",children:e.jsxs("div",{className:`relative bg-white shadow-2xl rounded-3xl p-8 max-[430px]:p-6 max-w-md w-full text-center\r
+                      scale-95 `,children:[e.jsx("div",{className:"absolute -top-6 -right-6 w-24 h-24 bg-yellow-500 rounded-full blur-3xl opacity-30"}),e.jsx("div",{className:"absolute -bottom-6 -left-6 w-24 h-24 bg-orange-500 rounded-full blur-3xl opacity-30"}),e.jsx("div",{className:`mx-auto w-24 h-24 max-[430px]:w-16 max-[430px]:h-16 flex items-center justify-center \r
+                        rounded-full bg-transparent text-white text-5xl max-[430px]:text-3xl mb-4  shadow-xl`,children:"⚠"}),e.jsx("h1",{className:"text-3xl max-[430px]:text-2xl font-bold text-yellow-600 tracking-wide",children:"Payment Cancelled"}),e.jsx("p",{className:"mt-4 text-gray-600 text-lg max-[430px]:text-sm leading-relaxed",children:"You cancelled the payment. Your order has not been placed."}),e.jsxs("div",{className:"flex flex-col justify-center gap-6 mt-8",children:[e.jsx(t,{to:"/checkout",className:`px-6 py-3 max-[430px]:px-4 max-[430px]:py-2\r
+                       bg-orange-600 text-white font-semibold rounded-lg\r
+                       hover:bg-orange-900 hover:-translate-y-1 hover:shadow-gray-500/60\r
+                       transition-all duration-300 max-[430px]:text-xs shadow-md`,children:"Go to cart"}),e.jsx(t,{to:"/checkout",className:`px-6 py-3 max-[430px]:px-4 max-[430px]:py-2\r
+                       bg-green-800 text-white font-semibold rounded-lg\r
+                       hover:bg-gray-900 hover:-translate-y-1 hover:shadow-gray-500/60\r
+                       transition-all duration-300 max-[430px]:text-xs shadow-md`,children:"Go Back to Checkout"})]})]})});export{x as default};
